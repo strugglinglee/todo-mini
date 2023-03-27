d5a2dd6f9b0d408c96ae7b7d40899681c95892ef
strugglinglee <strugglinglee@163.com>
2023-03-27 17:39:21 +0800

    feat: tabbar

---
+++ src/pages/mine/mine.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="content"> {{ title }} </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello')
+</script>
+
+<style lang="scss">
+.content {
+  background-color: antiquewhite;
+}
+</style>
